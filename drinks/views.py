7d4a028ba94325ca9d32baf8e4929b776d350f85
salvatoluice <salvatoluice5@gmail.com>
2024-01-04 16:07:19 +0300

    init api

--- drinks/views.py
+++ drinks/views.py
@@ -8,4 +8,4 @@ def drink_list(request):
     # gett all drinks, serialize them and return json
     drinks = Drink.objects.all()
     serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse(serializer.data)
+    return JsonResponse({"drinks": serializer.data}, safe=False)
